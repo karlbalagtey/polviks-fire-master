f5e2ee84cecaeb267837965a670a03113f6df9ff
Karl Mark Balagtey <karl.balagtey@gmail.com>
2021-02-17 16:26:39 +0000

    Updated server and package.json files

--- server.js
+++ server.js
@@ -22,11 +22,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(port, error => {
-    if (error) throw error;
-    console.log('Server running on port ' + port);
-});
-
 app.get('/service-worker.js', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
